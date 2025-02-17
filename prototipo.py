b8f1be12c8fd639df230f55e41abc1f1ee8919f1
Arthur <arthursantorogomes92@gmail.com>
2025-02-17 09:22:02 -0400

    terminei

--- prototipo.py
+++ prototipo.py
@@ -3,6 +3,7 @@ from kivy.uix.label import Label
 from kivy.uix.button import Button
 from kivy.uix.boxlayout import BoxLayout
 from kivy.uix.gridlayout import GridLayout
+from kivy.uix.image import Image
 
 from kivy.core.window import Window
 Window.size = (400,700)
@@ -11,8 +12,8 @@ class Mycalc(App):
     def build(self):
         layout = BoxLayout(orientation='vertical')
         self.display = Label(text='Bem vindo')
-        self.botao0= Button(text='zerar')
-        self.botao0.size_hint = (1.0,0.3)
+
+
         
         caixa_botoes= GridLayout(cols=4)
         self.botao1 = Button(text='0')
@@ -20,32 +21,44 @@ class Mycalc(App):
         self.botao3 = Button(text='2')
         self.botao4 = Button(text='3')
         self.botao5 = Button(text='4')
+        self.botao6 = Button(text='5')
+        self.botao7 = Button(text='6')
+        self.botao8 = Button(text='7')
+        self.botao9 = Button(text='8')
+        self.botao10 = Button(text='9')
         self.botao_mais = Button(text='+')
         self.botao_menos = Button(text='-')
         self.botao_vezes = Button(text='*')
         self.botao_divido = Button(text='/')
         self.botao_potencia = Button(text='^')
         self.botao_raiz4 = Button(text='√4')
-        self.botao_igual = Button(text='=')        
-
+        self.botao_igual = Button(text='=')
+        self.botao0= Button(text='C')
+        self.botao_delete1 = Button(text='<-')
 
         caixa_botoes.add_widget(self.botao1)
         caixa_botoes.add_widget(self.botao2)
         caixa_botoes.add_widget(self.botao3)
         caixa_botoes.add_widget(self.botao4)
         caixa_botoes.add_widget(self.botao5)
+        caixa_botoes.add_widget(self.botao6)
+        caixa_botoes.add_widget(self.botao7)
+        caixa_botoes.add_widget(self.botao8)
+        caixa_botoes.add_widget(self.botao9)
+        caixa_botoes.add_widget(self.botao10)
         caixa_botoes.add_widget(self.botao_mais)
         caixa_botoes.add_widget(self.botao_menos)
         caixa_botoes.add_widget(self.botao_vezes)
         caixa_botoes.add_widget(self.botao_divido)
         caixa_botoes.add_widget(self.botao_potencia)
         caixa_botoes.add_widget(self.botao_raiz4)
+        caixa_botoes.add_widget(self.botao0)
+        caixa_botoes.add_widget(self.botao_delete1)
         caixa_botoes.add_widget(self.botao_igual)
 
 
         layout.add_widget(self.display)
         layout.add_widget(caixa_botoes)
-        layout.add_widget(self.botao0)
 
         self.botao0.bind(on_press = self.zerar)
         self.botao1.bind(on_press = self.armazenar)
@@ -53,6 +66,11 @@ class Mycalc(App):
         self.botao3.bind(on_press = self.armazenar)
         self.botao4.bind(on_press = self.armazenar)
         self.botao5.bind(on_press = self.armazenar)
+        self.botao6.bind(on_press = self.armazenar)
+        self.botao7.bind(on_press = self.armazenar)
+        self.botao8.bind(on_press = self.armazenar)
+        self.botao9.bind(on_press = self.armazenar)
+        self.botao10.bind(on_press = self.armazenar)
         self.botao_mais.bind(on_press = self.armazenar)
         self.botao_menos.bind(on_press = self.armazenar)
         self.botao_vezes.bind(on_press = self.armazenar)
@@ -60,6 +78,7 @@ class Mycalc(App):
         self.botao_potencia.bind(on_press = self.armazenar)
         self.botao_raiz4.bind(on_press = self.armazenar)
         self.botao_igual.bind(on_press = self.calcular)
+        self.botao_delete1.bind(on_press = self.apagar)
 
 
         return layout
@@ -67,6 +86,11 @@ class Mycalc(App):
     def zerar(self,event):
         self.display.text = ''
 
+    def apagar(self,event):
+         if self.display.text:
+          current_text = self.display.text
+          self.display.text = current_text[:-1]
+    
     def armazenar(self,event):
         self.display.text += event.text
 
